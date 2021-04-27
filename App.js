3ab96077835a6ae0bf8ed66164b63778b6590ba5
Agyemang Duah Clinton <42769351+codekojo@users.noreply.github.com>
2021-04-27 17:33:25 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+export default App;
